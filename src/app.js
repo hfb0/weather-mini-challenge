e14b4c15d2563f19d34fd2e8e53fa4808be2f9cd
Henrique Barbosa <mudgenone@gmail.com>
2020-04-08 14:09:19 -0300

    refactor: isolate server startup

--- src/app.js
+++ src/app.js
@@ -1,20 +1,27 @@
 const express = require('express')
-const app = express()
 
-// Import route
 const weatherRoute = require('./routes/weather')
 
-// Use view engine
-app.set('view engine', 'ejs')
-app.set('views', './src/views')
+class App {
+  constructor() {
+    this.server = express()
 
-// Middleware
-app.use(express.static('./src/public'))
-app.use(express.urlencoded({ extended: true }))
-app.use('/', weatherRoute)
+    this.middlewares()
+    this.routes()
+  }
 
-const PORT = process.env.PORT || 5000
+  middlewares() {
+    // Use view engine
+    this.server.set('view engine', 'ejs')
+    this.server.set('views', './src/views')
 
-app.listen(PORT, () => {
-  console.log(`Server starting at port ${PORT}`)
-})
+    this.server.use(express.static('./src/public'))
+    this.server.use(express.urlencoded({ extended: true }))
+  }
+
+  routes() {
+    this.server.use('/', weatherRoute)
+  }
+}
+
+module.exports = new App().server
